46197f385980971c16a43a661bfcab95f7e27384
hxrizxnqq <125192601+hxrizxnqq@users.noreply.github.com>
2024-01-24 02:57:14 +0200

    added themes

--- js/app.js
+++ js/app.js
@@ -2,3 +2,21 @@ function hidePhoneNumber() {
   document.getElementById("phone-number").hidden = true;
 }
 hidePhoneNumber();
+
+
+var dark = false;
+var allElements = document.body;
+var switchButton = document.getElementById('theme-switch');
+
+function themeColors() {
+  if (!dark) {
+    allElements.className = 'theme-dark';
+    switchButton.innerHTML = 'Switch to light mode';
+    switchButton.style.color = '#ffffff';
+  } else {
+    allElements.className = 'theme-light';
+    switchButton.innerHTML = 'Switch to dark mode';
+    switchButton.style.color = "#000000";
+  }
+  dark = !dark;
+}
